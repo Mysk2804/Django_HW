a94a5d34a19376f85daf4f01a7e3c62242f01b6a
Mysk2804 <mysk2804@gmail.com>
2022-11-02 16:35:15 +1000

    4commit

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
 
+
 class Sensor(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
 
     def __str__(self):
         return f'Датчик {self.name}, Описание {self.description}'
+
 
 class Measurement(models.Model):
     id_sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,22 +1,22 @@
 # # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
+from rest_framework.generics import CreateAPIView, ListCreateAPIView, RetrieveUpdateAPIView
 
 from measurement.models import Sensor, Measurement
-from measurement.serializers import SensorDetailSerializer, MeasurementSerializer, SensorSerializer, MeasurementUpSerializer
+from measurement.serializers import SensorDetailSerializer, SensorSerializer, MeasurementUpSerializer
 
 
-class SensorView(ListAPIView):
-        queryset = Sensor.objects.all()
-        serializer_class = SensorSerializer
+class SensorView(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
 
 
-class MetaView(RetrieveAPIView):
-        queryset = Sensor.objects.all()
-        serializer_class = SensorDetailSerializer
+class MetaView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
 
 
 class NewTemp(CreateAPIView):
-        queryset = Measurement.objects.all()
-        serializer_class = MeasurementUpSerializer
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementUpSerializer
 
