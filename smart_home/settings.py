3f64af5d6eba3109be64943be19df72e218a630f
Sergey <mysk2804@gmail.com>
2023-01-09 16:03:40 +1000

    first commit

--- smart_home/settings.py
+++ smart_home/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9m2%u2yq=+#)6c#^j7^1p+@mpiob-r)9ki+zrv%*ad779fz89l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['95.163.235.8']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'smart_home.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'smart_home',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-        'USER': 'sergey',
-        'PASSWORD': '123456',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
